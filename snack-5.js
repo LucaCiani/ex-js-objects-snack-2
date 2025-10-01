7d648bfbeb41bb5f59ecc7e25b6be324422acfa3
LucaCiani <lucaciani88@gmail.com>
2025-10-01 13:34:40 +0200

    snack 5 complete

---
+++ snack-5.js
@@ -0,0 +1,31 @@
+// 3 oggetti
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29,
+    },
+};
+
+const newRestaurant = { ...hamburger.maker.restaurant }; // 1 nuovo oggetto
+newRestaurant.name = "Hyur's II";
+newRestaurant.address = "Second Street, 12";
+
+const secondBurger = { ...hamburger }; // 1 nuovo oggetti
+secondBurger.maker.restaurant = newRestaurant;
+secondBurger.maker.name = "Chef Hyur";
+
+console.log(hamburger.maker.name); // Chef Hyur
+console.log(secondBurger.maker.name); // Chef Hyur
+console.log(hamburger.maker.restaurant.name); // Hyur's II
+console.log(secondBurger.maker.restaurant.name); // Hyur's II
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+// 3 nell'oggetto originale, 1 per il nuovo ristorante, 1 per il nuovo hamburger, quindi in totale 5
