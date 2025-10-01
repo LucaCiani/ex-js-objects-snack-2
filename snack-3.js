8f4f782b0755136d075cecd63821de2c0e991420
LucaCiani <lucaciani88@gmail.com>
2025-10-01 13:13:45 +0200

    snack 3 complete

---
+++ snack-3.js
@@ -0,0 +1,19 @@
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29,
+    },
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// 9, 3 nell'oggetto originale e rispettivamente 3 per copia, in questo caso essendoci due copie salvate il risultato fa 9 oggetti 3+3+3
